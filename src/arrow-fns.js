19d8ef7361d675c1ae83341a46dd82dbff1789c8
Tyler Olsen <tylerkolsen@gmail.com>
2024-08-09 09:41:53 -0600

    completion of arrow functions section

--- src/arrow-fns.js
+++ src/arrow-fns.js
@@ -5,7 +5,7 @@
 // Ex.:
 //   giveMeTwo();
 //   => 2
-const giveMeTwo = undefined;
+const giveMeTwo = () => 2;
 
 // Set the value of addNums to an arrow function that takes in two numbers and
 // returns their sum.
@@ -13,7 +13,7 @@ const giveMeTwo = undefined;
 // Ex.:
 //   addNums(1, 2);
 //   => 3
-const addNums = undefined;
+const addNums = (num1, num2) => num1 + num2;
 
 // Set the value of max to an arrow function that takes in two numbers and
 // returns the largest one.
@@ -21,7 +21,7 @@ const addNums = undefined;
 // Ex.:
 //   max(1, 2);
 //   => 2
-const max = undefined;
+const max = (num1, num2) => num1 > num2 ? num1 : num2;
 
 // Given an array of numbers, return a new array that only includes the even
 // numbers. Do this using array iteration methods. Do NOT use a for loop.
@@ -29,7 +29,9 @@ const max = undefined;
 // Ex.:
 //   evens([1, 2, 3, 4, 5]);
 //   => [2, 4]
-function evens(nums) {}
+function evens(nums) {
+    return nums.filter((arrNum) => arrNum % 2 === 0);
+  }
 
 // Given an array of names, return a new array of greetings for each name. Do
 // this using array iteration methods. Do NOT use a for loop.
@@ -37,7 +39,11 @@ function evens(nums) {}
 // Ex.:
 //   createGreetings(['Clive', 'Jill', 'Torgal']);
 //   => ['Hello, Clive!', 'Hello, Jill!', 'Hello, Torgal!']
-function createGreetings(names) {}
+function createGreetings(names) {
+    return names.map((arrName) => {
+        return `Hello, ${arrName}!`
+    })
+}
 
 // Given an array of words, return a new array where each word is uppercased
 // and only includes words that are longer than 4 characters. Do this using
@@ -46,6 +52,8 @@ function createGreetings(names) {}
 // Ex.:
 //   loudLongWords(['apple', 'pear', 'cake', 'pinata']);
 //   => ['APPLE', 'PINATA']
-function loudLongWords(words) {}
+function loudLongWords(words) {
+    return words.map((word) => word.toUpperCase()).filter((word) => word.length > 4)
+}      
 
 export { addNums, createGreetings, evens, giveMeTwo, loudLongWords, max };
